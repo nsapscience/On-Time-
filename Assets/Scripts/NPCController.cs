33e9b284bd1efdd290a98a549707450d432ca86a
nsapscience <my5y8gf79p@privaterelay.appleid.com>
2026-03-10 13:33:51 +0100

    Menü und Pause

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class NewMonoBehaviourScript : MonoBehaviour, Interactable
 {
     [SerializeField] Dialog dialog;
-    [SerializeField] GameObject choiceUI; // Ein Panel mit "Ja" und "Nein" Buttons
-    [SerializeField] GameObject targetWindow; // Das eigentliche Fenster (z.B. Shop/Minispiel)
 
     public void Interact()
     {
@@ -18,24 +16,5 @@ public class NewMonoBehaviourScript : MonoBehaviour, Interactable
         // 1. Warte, bis der DialogManager fertig ist
         yield return StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
 
-        // 2. Dialog ist fertig -> Zeige die Auswahl-Buttons (Ja/Nein)
-        if (choiceUI != null)
-        {
-            choiceUI.SetActive(true);
-        }
-    }
-
-    // Diese Funktion wird vom "Ja"-Button aufgerufen
-    public void OnAcceptChoice()
-    {
-        choiceUI.SetActive(false);
-        if (targetWindow != null) targetWindow.SetActive(true);
-    }
-
-    // Diese Funktion wird vom "Nein"-Button aufgerufen
-    public void OnDeclineChoice()
-    {
-        choiceUI.SetActive(false);
-        Debug.Log("Spieler hat abgelehnt.");
     }
 }
